9a40970bf8d957260e74a691a8d0d2b13fb6a638
Riki <ri.khresna@gmail.com>
2023-09-05 21:03:12 +0700

    Tugas SQL Database Alias

---
+++ _laskar_pelangi__202309052101.php
@@ -0,0 +1,154 @@
+<?php
+$select Name as Anggota, Phone as Nomor_Telepon, Adress as Rumah_Tinggal from `laskar pelangi` = array(
+	array(
+		"Anggota" => "Lazuardi",
+		"Nomor_Telepon" => "02511234567",
+		"Rumah_Tinggal" => "Belakang mall jogja"
+	),
+	array(
+		"Anggota" => "Adhitiyanto",
+		"Nomor_Telepon" => "02511234577",
+		"Rumah_Tinggal" => "Bogor Utara II"
+	),
+	array(
+		"Anggota" => "Tine Widya",
+		"Nomor_Telepon" => "02511234587",
+		"Rumah_Tinggal" => "Bangbarung"
+	),
+	array(
+		"Anggota" => "Annisa Shafira",
+		"Nomor_Telepon" => "02511236549",
+		"Rumah_Tinggal" => "Cimanggu permai"
+	),
+	array(
+		"Anggota" => "Sarah Pramiarsih",
+		"Nomor_Telepon" => "02513214567",
+		"Rumah_Tinggal" => "Yasmin sektor IV"
+	),
+	array(
+		"Anggota" => "Klinsmann Utama",
+		"Nomor_Telepon" => "02513212315",
+		"Rumah_Tinggal" => "Jl. Bali no.3"
+	),
+	array(
+		"Anggota" => "Khrisnindya Randhu",
+		"Nomor_Telepon" => "02514561234",
+		"Rumah_Tinggal" => "Pangranggo utara dalam"
+	),
+	array(
+		"Anggota" => "Aziola Dharma",
+		"Nomor_Telepon" => "02517698933",
+		"Rumah_Tinggal" => "kemang prapatan"
+	),
+	array(
+		"Anggota" => "Rara Ayu",
+		"Nomor_Telepon" => "02519988776",
+		"Rumah_Tinggal" => "Gunung batu"
+	),
+	array(
+		"Anggota" => "Dika Adhira",
+		"Nomor_Telepon" => "02511520090",
+		"Rumah_Tinggal" => "Budi agung II"
+	),
+	array(
+		"Anggota" => "Nirmala Eka",
+		"Nomor_Telepon" => "02517689541",
+		"Rumah_Tinggal" => "Budi agung I"
+	),
+	array(
+		"Anggota" => "Shelly Noviana",
+		"Nomor_Telepon" => "02517698090",
+		"Rumah_Tinggal" => "Bumi Kedoya Indah"
+	),
+	array(
+		"Anggota" => "Viviyona A",
+		"Nomor_Telepon" => "02518786564",
+		"Rumah_Tinggal" => "BDB 1 blok FA"
+	),
+	array(
+		"Anggota" => "Putri Ayu",
+		"Nomor_Telepon" => "02512345776",
+		"Rumah_Tinggal" => "Jl. Kejora"
+	),
+	array(
+		"Anggota" => "Tita Annisa",
+		"Nomor_Telepon" => "02514545341",
+		"Rumah_Tinggal" => "Sentul villa"
+	),
+	array(
+		"Anggota" => "Handal H",
+		"Nomor_Telepon" => "02516669870",
+		"Rumah_Tinggal" => "BDB I blok FG"
+	),
+	array(
+		"Anggota" => "Devi Muthia",
+		"Nomor_Telepon" => "02511238764",
+		"Rumah_Tinggal" => "Villa bogor indah"
+	),
+	array(
+		"Anggota" => "Aida Septiana",
+		"Nomor_Telepon" => "02518848487",
+		"Rumah_Tinggal" => "pura bojonggede blok K"
+	),
+	array(
+		"Anggota" => "Praditya T",
+		"Nomor_Telepon" => "02510070089",
+		"Rumah_Tinggal" => "Purwakarta"
+	),
+	array(
+		"Anggota" => "Kendy Rizki",
+		"Nomor_Telepon" => "02511123765",
+		"Rumah_Tinggal" => "Bukit asam jaya I"
+	),
+	array(
+		"Anggota" => "Dewinta Kiswi",
+		"Nomor_Telepon" => "02514436732",
+		"Rumah_Tinggal" => "Bogor baru"
+	),
+	array(
+		"Anggota" => "Tito Utomo",
+		"Nomor_Telepon" => "02515545538",
+		"Rumah_Tinggal" => "Yasmin sektor V"
+	),
+	array(
+		"Anggota" => "Elysia I",
+		"Nomor_Telepon" => "02515532184",
+		"Rumah_Tinggal" => "Koramil pintu selatan\/depan Indomaret"
+	),
+	array(
+		"Anggota" => "Putri Aprilia",
+		"Nomor_Telepon" => "02516664325",
+		"Rumah_Tinggal" => "Talang dalam no.16"
+	),
+	array(
+		"Anggota" => "Aulia Maisun",
+		"Nomor_Telepon" => "02517670543",
+		"Rumah_Tinggal" => "Depok permai gang Kuda"
+	),
+	array(
+		"Anggota" => "Rangga Surakusuma",
+		"Nomor_Telepon" => "02510010010",
+		"Rumah_Tinggal" => "Antasari"
+	),
+	array(
+		"Anggota" => "Nisfal Filsa",
+		"Nomor_Telepon" => "02519980003",
+		"Rumah_Tinggal" => "Baranangsiang indah"
+	),
+	array(
+		"Anggota" => "Riani Amalia",
+		"Nomor_Telepon" => "02517698892",
+		"Rumah_Tinggal" => "Jl. Mayjen Prapto"
+	),
+	array(
+		"Anggota" => "Bagus Garnadi",
+		"Nomor_Telepon" => "02515532346",
+		"Rumah_Tinggal" => "BDB 1 Blok FD"
+	),
+	array(
+		"Anggota" => "Ogiano Waskita",
+		"Nomor_Telepon" => "02513216547",
+		"Rumah_Tinggal" => "Ciomas"
+	)
+);
+?>
